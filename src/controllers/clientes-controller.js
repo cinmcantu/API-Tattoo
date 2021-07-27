a5d20d2ba16d6cbc22010547d05a6be4466af88b
barbaraavs <barbara.avelar06@gmail.com>
2021-07-26 23:09:07 -0300

    Alterações GET - Consulta individual

--- src/controllers/clientes-controller.js
+++ src/controllers/clientes-controller.js
@@ -14,55 +14,15 @@ module.exports= (app, bd)=>{
         }
     })
 
-//     app.get('/pets/dogs/:ID', async (req, res)=>{
-//         try{
+    app.get('/clientes/dados/:ID', async (req, res)=>{
+        try{
 
-//             const id = req.params.ID
-//             const respostaVerUmDog = await DaoDogs.VerUmDog(id)
-//             res.send(respostaVerUmDog)
+            const id = req.params.ID
+            const respostaVerUmCliente = await DaoClientes.VerUmCliente(id)
+            res.send(respostaVerUmCliente)
 
-//         }catch(error){
-//             res.send(error)
-//         }
-//     })
-    
-//     app.post('/pets/dogs/newDog', async(req, res)=>{
-//         try{
-//             const body = req.body
-//             const infos = [body.RACA, body.FOTO, body.IDADE, body.NOME, body.GENERO, body.RUA, body.NUMERO, body.CIDADE, body.ESTADO, body.TELEFONE]
-
-//             const respostaNewDog = await DaoDogs.NewDog(infos)
-//             res.send(respostaNewDog)
-
-//         }catch(error){
-//             res.send(error)
-//         }
-//     })
-
-//     app.delete('/pets/dogs/delete/:ID', async(req, res)=>{
-//         try{
-//             const id = req.params.ID
-//             const respostaDeleteDogs = await DaoDogs.DeleteDogs(id)
-//             res.send(respostaDeleteDogs)
-//         }catch(error){
-//             res.send(error)
-//         }
-//     })
-
-//     app.put('/pets/dogs/edit/:ID', async(req, res)=>{
-//         try{
-
-//             const id = req.params.ID
-
-//             const body = req.body
-//             const infos = [body.RACA, body.FOTO, body.IDADE, body.NOME, body.GENERO, body.RUA, body.NUMERO, body.CIDADE, body.ESTADO, body.TELEFONE]
-
-//             const respostaEditDog = await DaoDogs.EditDog(infos, id)
-
-//             res.send(respostaEditDog)
-
-//         }catch(error){
-//             res.send(error)
-//         }
-//     })
+        }catch(error){
+            res.send(error)
+        }
+    })
 }
--- src/dao/clientes-dao.js
+++ src/dao/clientes-dao.js
@@ -9,51 +9,19 @@ module.exports = class ClientesDao{
         return new Promise((resolve, reject)=>{
             const query = 'SELECT * FROM CLIENTES'
             this.bd.all(query, (error, response)=>{
-                if(error) reject(`Erro ao acessar o BD. ${error}`)
+                if(error) reject(`Erro ao acessar o Banco de Dados. ${error}`)
                 else resolve(response)
             })
         })
     }
 
-    // VerUmDog(id){
-    //     return new Promise((resolve, reject)=>{
-    //         const query = 'SELECT * FROM DOGS WHERE ID= (?)'
-    //         this.bd.all(query, id,(error, response)=>{
-    //             if(error) reject(`Erro ao acessar o BD. ${error}`)
-    //             else resolve(response)
-    //         } )
-    //     })
-    // }
-
-    // NewDog(infos){
-    //     return new Promise((resolve, reject)=>{
-    //         const query = 'INSERT INTO DOGS (RACA, FOTO, IDADE, NOME, GENERO, RUA, NUMERO, CIDADE, ESTADO, TELEFONE) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
-    //         const parametros = [infos[0], infos[1], infos[2], infos[3], infos[4], infos[5], infos[6], infos[7], infos[8], infos[9]]
-    //         this.bd.run(query, parametros, (error, response)=>{
-    //             if(error) reject(`Erro ao adicionar dog. ${error}`)
-    //             else resolve('Dog adicionado com sucesso')
-    //         })
-    //     })
-    // }
-
-    // DeleteDogs(id){
-    //     return new Promise((resolve, reject)=>{
-    //         const query = 'DELETE FROM DOGS WHERE ID = (?)'
-    //         this.bd.run(query, id, (error, response)=>{
-    //             if(error) reject(`Erro ao excluir dog. ${error}`)
-    //             else resolve("Dog excluido")
-    //         })
-    //     })
-    // }
-
-    // EditDog(infos, id){
-    //     return new Promise((resolve, reject)=>{
-    //         const query = 'UPDATE DOGS SET RACA = (?), FOTO = (?), IDADE = (?), NOME = (?), GENERO = (?), RUA = (?), NUMERO = (?), CIDADE = (?), ESTADO = (?), TELEFONE = (?) WHERE ID = (?)'
-    //         const parametros = [infos[0], infos[1], infos[2], infos[3], infos[4], infos[5], infos[6], infos[7], infos[8], infos[9],id]
-    //         this.bd.run(query, parametros, (error, response)=>{
-    //             if(error) reject(`Erro ao editar dog. ${error}`)
-    //             else resolve("Dog Editado")
-    //         })
-    //     })
-    // }
+    VerUmCliente(id){
+        return new Promise((resolve, reject)=>{
+            const query = 'SELECT * FROM CLIENTES WHERE ID= (?)'
+            this.bd.all(query, id,(error, response)=>{
+                if(error) reject(`Erro ao acessar o Banco de Dados. ${error}`)
+                else resolve(response)
+            } )
+        })
+    }
 }
