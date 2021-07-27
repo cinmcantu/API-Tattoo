529491cde2cc60ea0de9481972b786fc43f36825
barbaraavs <barbara.avelar06@gmail.com>
2021-07-26 23:48:37 -0300

    Ajuste identação

--- src/controllers/clientes-controller.js
+++ src/controllers/clientes-controller.js
@@ -1,69 +1,69 @@
 const ClientesDao = require('../dao/clientes-dao')
 module.exports= (app, bd)=>{
 
-    const DaoClientes = new ClientesDao(bd)
+const DaoClientes = new ClientesDao(bd)
 
-    app.get('/clientes/dados', async (req, res)=>{
-        try{
+app.get('/clientes/dados', async (req, res)=>{
+    try{
 
-            const respostaVerClientes = await DaoClientes.VerClientes()
-            res.send(respostaVerClientes)
+        const respostaVerClientes = await DaoClientes.VerClientes()
+        res.send(respostaVerClientes)
 
-        }catch(error){
-            res.send(error)
-        }
-    })
+    }catch(error){
+        res.send(error)
+    }
+})
 
-    app.get('/clientes/dados/:ID', async (req, res)=>{
-        try{
+app.get('/clientes/dados/:ID', async (req, res)=>{
+    try{
 
-            const id = req.params.ID
-            const respostaVerUmCliente = await DaoClientes.VerUmCliente(id)
-            res.send(respostaVerUmCliente)
+        const id = req.params.ID
+        const respostaVerCliente = await DaoClientes.VerCliente(id)
+        res.send(respostaVerCliente)
 
-        }catch(error){
-            res.send(error)
-        }
-    })
+    }catch(error){
+        res.send(error)
+    }
+})
 
-    app.post('/clientes/dados/novoCliente', async(req, res)=>{
-        try{
-            const body = req.body
-            const infos = [body.NOME, body.IDADE, body.GENERO, body.RUA, body.NUMERO, body.CIDADE, body.ESTADO, body.TELEFONE, body.TATUADOR, body.DATA_CADASTRO]
+app.post('/clientes/dados/novoCliente', async(req, res)=>{
+    try{
+        const body = req.body
+        const infos = [body.NOME, body.IDADE, body.GENERO, body.RUA, body.NUMERO, body.CIDADE, body.ESTADO, body.TELEFONE, body.TATUADOR, body.DATA_CADASTRO]
         
-            const respostaNovoCliente = await DaoClientes.NovoCliente(infos)
-            res.send(respostaNovoCliente)
+        const respostaNovoCliente = await DaoClientes.NovoCliente(infos)
+        res.send(respostaNovoCliente)
         
-        }catch(error){
-            res.send(error)
-        }
-    })
+    }catch(error){
+        res.send(error)
+    }
+})
 
-    app.delete('/clientes/dados/delete/:ID', async(req, res)=>{
-        try{
-            const id = req.params.ID
-            const respostaDeleteCliente = await DaoClientes.DeleteCliente(id)
-            res.send(respostaDeleteCliente)
-        }catch(error){
-            res.send(error)
-        }
-    })
+app.delete('/clientes/dados/delete/:ID', async(req, res)=>{
+    try{
+        const id = req.params.ID
+        const respostaDeleteCliente = await DaoClientes.DeleteCliente(id)
+        res.send(respostaDeleteCliente)
+    }catch(error){
+        res.send(error)
+    }
+})
  
-    app.put('/clientes/dados/edit/:ID', async(req, res)=>{
-        try{
+app.put('/clientes/dados/edit/:ID', async(req, res)=>{
+    try{
 
-            const id = req.params.ID
+        const id = req.params.ID
 
-            const body = req.body
-            const infos = [body.NOME, body.IDADE, body.GENERO, body.RUA, body.NUMERO, body.CIDADE, body.ESTADO, body.TELEFONE, body.TATUADOR, body.DATA_CADASTRO]
+        const body = req.body
+        const infos = [body.NOME, body.IDADE, body.GENERO, body.RUA, body.NUMERO, body.CIDADE, body.ESTADO, body.TELEFONE, body.TATUADOR, body.DATA_CADASTRO]
 
-            const respostaEditCliente = await DaoClientes.EditCliente(infos, id)
+        const respostaEditCliente = await DaoClientes.EditCliente(infos, id)
 
-            res.send(respostaEditCliente)
+        res.send(respostaEditCliente)
 
-        }catch(error){
-            res.send(error)
-        }
-    })
+    }catch(error){
+        res.send(error)
+    }
+})
 
 }
--- src/dao/clientes-dao.js
+++ src/dao/clientes-dao.js
@@ -5,56 +5,56 @@ module.exports = class ClientesDao{
     }
 
 
-    VerClientes(){
-        return new Promise((resolve, reject)=>{
-            const query = 'SELECT * FROM CLIENTES'
-            this.bd.all(query, (error, response)=>{
-                if(error) reject(`Erro ao acessar o Banco de Dados. ${error}`)
-                else resolve(response)
-            })
+VerClientes(){
+    return new Promise((resolve, reject)=>{
+        const query = 'SELECT * FROM CLIENTES'
+        this.bd.all(query, (error, response)=>{
+            if(error) reject(`Erro ao acessar o Banco de Dados. ${error}`)
+            else resolve(response)
         })
-    }
+    })
+}
 
-    VerUmCliente(id){
-        return new Promise((resolve, reject)=>{
-            const query = 'SELECT * FROM CLIENTES WHERE ID = (?)'
-            this.bd.all(query, id,(error, response)=>{
-                if(error) reject(`Erro ao acessar o Banco de Dados. ${error}`)
-                else resolve(response)
-            } )
+VerCliente(id){
+    return new Promise((resolve, reject)=>{
+        const query = 'SELECT * FROM CLIENTES WHERE ID = (?)'
+        this.bd.all(query, id,(error, response)=>{
+            if(error) reject(`Erro ao acessar o Banco de Dados. ${error}`)
+            else resolve(response)
         })
-    }
+    })
+}
 
-    NovoCliente(infos){
-        return new Promise((resolve, reject)=>{
-            const query = 'INSERT INTO CLIENTES (NOME, IDADE, GENERO, RUA, NUMERO, CIDADE, ESTADO, TELEFONE, TATUADOR, DATA_CADASTRO) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
-            const parametros = [infos[0], infos[1], infos[2], infos[3], infos[4], infos[5], infos[6], infos[7], infos[8], infos[9]]
-            this.bd.run(query, parametros, (error, response)=>{
-                if(error) reject(`Erro ao adicionar cliente. ${error}`)
-                else resolve('Cliente adicionado com sucesso')
-            })
+NovoCliente(infos){
+    return new Promise((resolve, reject)=>{
+        const query = 'INSERT INTO CLIENTES (NOME, IDADE, GENERO, RUA, NUMERO, CIDADE, ESTADO, TELEFONE, TATUADOR, DATA_CADASTRO) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+        const parametros = [infos[0], infos[1], infos[2], infos[3], infos[4], infos[5], infos[6], infos[7], infos[8], infos[9]]
+        this.bd.run(query, parametros, (error, response)=>{
+            if(error) reject(`Erro ao adicionar cliente. ${error}`)
+            else resolve('Cliente adicionado com sucesso')
         })
-    }
+    })
+}
 
-    DeleteCliente(id){
-        return new Promise((resolve, reject)=>{
-            const query = 'DELETE FROM CLIENTES WHERE ID = (?)'
-            this.bd.run(query, id, (error, response)=>{
-                if(error) reject(`Erro ao excluir Cliente. ${error}`)
-                else resolve("Cliente excluido")
-            })
+DeleteCliente(id){
+    return new Promise((resolve, reject)=>{
+        const query = 'DELETE FROM CLIENTES WHERE ID = (?)'
+        this.bd.run(query, id, (error, response)=>{
+            if(error) reject(`Erro ao excluir Cliente. ${error}`)
+            else resolve("Cliente excluido")
         })
-    }
+    })
+}
 
-    EditCliente(infos, id){
-        return new Promise((resolve, reject)=>{
-            const query = 'UPDATE CLIENTES SET NOME = (?), IDADE = (?), GENERO = (?), RUA = (?), NUMERO = (?), CIDADE = (?), ESTADO = (?), TELEFONE = (?), TATUADOR = (?), DATA_CADASTRO = (?) WHERE ID = (?)'
-            const parametros = [infos[0], infos[1], infos[2], infos[3], infos[4], infos[5], infos[6], infos[7], infos[8], infos[9],id]
-            this.bd.run(query, parametros, (error, response)=>{
-                if(error) reject(`Erro ao editar Cliente. ${error}`)
-                else resolve("Cliente Editado")
-            })
+EditCliente(infos, id){
+    return new Promise((resolve, reject)=>{
+        const query = 'UPDATE CLIENTES SET NOME = (?), IDADE = (?), GENERO = (?), RUA = (?), NUMERO = (?), CIDADE = (?), ESTADO = (?), TELEFONE = (?), TATUADOR = (?), DATA_CADASTRO = (?) WHERE ID = (?)'
+        const parametros = [infos[0], infos[1], infos[2], infos[3], infos[4], infos[5], infos[6], infos[7], infos[8], infos[9],id]
+        this.bd.run(query, parametros, (error, response)=>{
+            if(error) reject(`Erro ao editar Cliente. ${error}`)
+            else resolve("Cliente Editado")
         })
-    }
+    })
+}
 
 }
